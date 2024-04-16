cd0b9fb1b126bf68a52a9aa8c6412e280724c30c
pchayapat <71373068+pchayapat@users.noreply.github.com>
2024-04-16 20:14:25 +0700

    Add water system

--- Assets/Scripts/Map - Control/WaterFlow.cs
+++ Assets/Scripts/Map - Control/WaterFlow.cs
@@ -6,10 +6,6 @@ public class WaterFlow : MonoBehaviour
 {
     public float speed;
     Rigidbody rb;
-
-    public bool back;
-    public bool right;
-
     void Start ()
     {
         rb = GetComponent<Rigidbody>();
@@ -18,20 +14,7 @@ public class WaterFlow : MonoBehaviour
     void FixedUpdate ()
     {
         Vector3 pos = rb.position;
-        if(back == true && right == false){
-            Back();
-        }
-        if(back == false && right == true){
-            Right();
-        }
-        //rb.position += Vector3.back * speed * Time.fixedDeltaTime;
-        rb.MovePosition(pos);
-    }
-
-    public void Back(){
         rb.position += Vector3.back * speed * Time.fixedDeltaTime;
-    }
-    public void Right(){
-        rb.position += Vector3.right * speed * Time.fixedDeltaTime;
+        rb.MovePosition(pos);
     }
 }
